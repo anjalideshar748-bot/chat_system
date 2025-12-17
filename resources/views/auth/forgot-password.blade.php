3218c905bfd6ed490e631ad9148f7e989e19331c
Anjali <anjalideshar748@gmail.com>
2025-12-17 08:39:00 +0545

    reset password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,32 @@
 <x-welcome>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+    <div class="bg-green-800 p-10 flex items-center">
+        <div class="w-full max-w-sm mx-auto text-white">
+            <h2 class="text-1xl font-bold mb-8"> {{ __('Forgot your password? ') }}</h2>
+            <h5 class="text-sm mb-8">
+                {{ __('No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            </h5>
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+                <!-- Email Address -->
+                <div>
+                    <div class="mb-5">
+                        <label class="block mb-2 text-sm text-white">Email</label>
+                        <input type="text" name="email" placeholder="Enter your email address"
+                            class="w-full px-4 py-3 rounded-full bg-green-700 text-white placeholder-green-300 focus:outline-none focus:ring-2 focus:ring-green-300 transition duration-300 hover:bg-green-600" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+                    <div class="flex items-center justify-end mt-4">
+                        <button
+                            class="w-full py-3 rounded-full bg-teal-400 text-green-900 font-semibold
+                 hover:bg-teal-300 hover:shadow-lg hover:scale-[1.02]
+                 active:scale-95 transition duration-300">Email
+                            Password Reset Link</button>
+                    </div>
+            </form>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-welcome>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-welcome>
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -7,33 +7,38 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+            <label class="block mb-2 text-sm text-white">Email</label>
+            <input type="text" name="email" placeholder="Enter your email address" :value="old('email', $request->email)"
+                class="w-full px-4 py-3 rounded-full bg-green-700 text-white placeholder-green-300 focus:outline-none focus:ring-2 focus:ring-green-300 transition duration-300 hover:bg-green-600" />
+            {{-- <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" /> --}}
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            <label class="block mb-2 text-sm text-white">Password</label>
+            {{-- <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" /> --}}
+             <input type="password" name="password" placeholder="Enter your password" :value="old('password', $request->password)"
+                class="w-full px-4 py-3 rounded-full bg-green-700 text-white placeholder-green-300 focus:outline-none focus:ring-2 focus:ring-green-300 transition duration-300 hover:bg-green-600" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <label class="block mb-2 text-sm text-white">Confirm Password</label>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+            <input type="password" name="password_confirmation" placeholder="Confirm your password" :value="old('password_confirmation', $request->password_confirmation)"
+                class="w-full px-4 py-3 rounded-full bg-green-700 text-white placeholder-green-300 focus:outline-none focus:ring-2 focus:ring-green-300 transition duration-300 hover:bg-green-600" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
+                        <button
+                            class="w-full py-3 rounded-full bg-teal-400 text-green-900 font-semibold
+                 hover:bg-teal-300 hover:shadow-lg hover:scale-[1.02]
+                 active:scale-95 transition duration-300">Email
+                            Reset Password</button>
+                    </div>
     </form>
-</x-guest-layout>
+</x-welcome>z
