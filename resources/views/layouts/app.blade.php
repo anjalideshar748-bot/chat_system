f0753234d9f0810a8774697ef78409cba181aa8f
Anjali <anjalideshar748@gmail.com>
2025-12-26 18:16:47 +0545

    ..

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,7 @@
 
       <div class="flex">
         <div class="bg-teal-100 px-4 py-2 rounded-2xl max-w-md">
-          Hey! what’s the update?
+          Hey! what’s up?
         </div>
       </div>
 
