7a919cd5ff8a5fc6f36e9fc0ea9618900bfa4c58
Anjali <anjalideshar748@gmail.com>
2025-12-27 20:33:05 +0545

    ..

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -174,8 +174,9 @@
 
     <!-- Logo -->
     <div class="p-5 flex items-center gap-3 bg-gradient-to-r from-teal-500 to-green-500 text-white">
-      <img src="7c03777d88ee2dffef97e812961c7b3d-removebg-preview.png"
-     class="w-10 h-10 rounded-full object-contain">
+    <img src="7c03777d88ee2dffef97e812961c7b3d-removebg-preview.png"
+     class="w-12 h-12 object-contain">
+
 
       <h1 class="text-xl font-bold">Talk</h1>
     </div>
