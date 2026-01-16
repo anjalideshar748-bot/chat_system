b057cf3d735dea5d6da9b50718700818ea9040c2
Anjali <anjalideshar748@gmail.com>
2026-01-16 17:15:26 +0545

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -475,4 +475,4 @@ document.querySelector('[data-chat="anjali"]').click();
 </html>
 
 
-{{-- // hello i am anjali deshar. i just like do whatever i want but i m not allowed and i feel so uckin--}}
+{{-- // hello i am anjali deshar. i just like do whatever i want but i m not allowed and i feelso uckin--}}
